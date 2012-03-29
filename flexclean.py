65b54adf6627467eac0d984b78b76b13eb3a93ec
Antti Maula <antti.maula@gmail.com>
2012-03-29 11:52:07 +0300

    First BETA version completed. Not yet SAFE to use on any real system.

--- flexclean.py
+++ flexclean.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python
 # -*- mode: python; coding: utf-8 -*-
+"""
+flexclean - A Really simple tool to clean up obsolete files from a flexbackup timestamped archive
+Copyright (C) 2012 Antti Maula
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+See <http://www.gnu.org/licenses/>.
+"""
+
+
 
 import argparse
 import os
 import re
+import sys
 from glob import glob
 
 def handle_arguments():
     """Handle command line parameters.
     """
     parser = argparse.ArgumentParser()
-    #parser.add_argument('-r', action='store', metavar='num', default=20, type=int,
-    #                    help='Number of corrects required before considering as "learned" (default: 20)')
-    #parser.add_argument('-m', action='store', metavar='num', default=0.4, type=float,
-    #                    help='Required mean delay between characters (default: 0.4)')
-    #parser.add_argument('-s', action='store', metavar='num', default=0.25, type=float,
-    #                    help='Required standard deviation between characters (default: 0.25)')
+    parser.add_argument('-p', action='store', metavar='path', default='.', type=str,
+                        help='Path to clean up (default: current directory)')
+    parser.add_argument('-m', action='store', metavar='wildcard', default='*.tar', type=str,
+                        help='Wildcard of files to match - only these will be checked (default: *.tar)')
+    parser.add_argument('-d', action='store', metavar='depth', default=1, type=int,
+                        help='Number of files per level to keep (default: 1)')
     #parser.add_argument('-i', action='store', metavar='num', default=5, type=int,
     #                    help='History length to consider when checking mean/std values (default: 5)')
     
@@ -31,9 +50,20 @@ def main():
     
     # Arguments
     arguments = handle_arguments()
+    path = arguments.p
+    match = arguments.m
+    keep_depth = arguments.d
+
+    # Stupid user check
+    assert keep_depth > 0
 
     # Get files
     files = glob( os.path.join(path, match) )
+    if len(files) == 0:
+        print "No matching (%s) files found from given path (%s) - will do nothing" % \
+              (match, path)
+        sys.exit(1)
+
 
     # split and organize
     filesbyprefix = {}
@@ -48,8 +78,6 @@ def main():
             files.append( ( timestamp, infile ) )
             filesbylevel[str(level)] = files
             filesbyprefix[prefix] = filesbylevel
-
-    print "Number of prefixes:", len(filesbyprefix), [ p for p in filesbyprefix ]
 
     # select for delete
     deleted_files = []
@@ -70,9 +98,13 @@ def main():
             kept_files.extend(to_keep)
 
     # Now print
-    print "Keeping files:"
-    for f in kept_files:
-        print f[1]
+    if len(kept_files) > 0:
+        print "Keeping files:"
+        for f in kept_files:
+            print f[1]
+    else:
+        print "No files to keep! This is probably an error, will not continue!"
+        sys.exit(1)
 
     if len(deleted_files) > 0:
         print "About to delete files:"
